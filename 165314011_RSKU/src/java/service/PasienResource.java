909a04835c3f96b18f2bfd650b7c51bd93e24e4d
wewen <wewen@wewen-PC>
2018-10-30 15:10:13 +0700

    Version 0.6

--- 165314011_RSKU/src/java/service/PasienResource.java
+++ 165314011_RSKU/src/java/service/PasienResource.java
@@ -5,6 +5,9 @@
  */
 package service;
 
+import com.google.gson.Gson;
+import helper.PasienHelper;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -13,6 +16,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.eclipse.jdt.internal.compiler.util.Util;
+import pojos.Pasien;
 
 /**
  * REST Web Service
@@ -33,17 +39,26 @@ public class PasienResource {
 
     /**
      * Retrieves representation of an instance of service.PasienResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
+    public Response getJson() {
         //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        PasienHelper helper = new PasienHelper();
+        List<Pasien> list = helper.getAllPasien();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return Response.status(200)
+                .entity(json)
+                .build();
+
     }
 
     /**
      * PUT method for updating or creating an instance of PasienResource
+     *
      * @param content representation for the resource
      */
     @PUT
