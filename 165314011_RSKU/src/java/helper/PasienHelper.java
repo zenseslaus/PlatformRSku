d983c8b777b952e6000cb71c4a180bf008d1f887
wewen <wewen@wewen-PC>
2018-10-30 14:54:46 +0700

    Version 0.4

---
+++ 165314011_RSKU/src/java/helper/PasienHelper.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import pojos.Pasien;
+import util.RSKUHibernateUtil;
+
+/**
+ *
+ * @author wewen
+ */
+public class PasienHelper {
+
+    public PasienHelper() {
+    }
+
+    public List<Pasien> getAllPasien() {
+        List<Pasien> result = null;
+        Session session = RSKUHibernateUtil.getSessionFactory().openSession();
+        String query = "from Pasien p";
+        Query q = session.createQuery(query);
+        result = q.list();
+        session.close();
+        return result;
+    }
+}
