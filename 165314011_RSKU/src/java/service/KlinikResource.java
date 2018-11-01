c479b597b991140378956ad57c343ad6f57c55d2
wewen <wewen@wewen-PC>
2018-11-01 15:08:07 +0700

    Version 0.10

--- 165314011_RSKU/src/java/service/KlinikResource.java
+++ 165314011_RSKU/src/java/service/KlinikResource.java
@@ -5,6 +5,9 @@
  */
 package service;
 
+import com.google.gson.Gson;
+import helper.KlinikHelper;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -13,6 +16,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pojos.Klinik;
 
 /**
  * REST Web Service
@@ -33,21 +38,28 @@ public class KlinikResource {
 
     /**
      * Retrieves representation of an instance of service.KlinikResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public Response getJson() {
+        KlinikHelper helper = new KlinikHelper();
+        List<Klinik> list = helper.getAllKlinik();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return Response.status(200)
+                .entity(json)
+                .build();
     }
 
     /**
      * PUT method for updating or creating an instance of KlinikResource
+     *
      * @param content representation for the resource
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
-    }
+//    @PUT
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public void putJson(String content) {
+//    }
 }
