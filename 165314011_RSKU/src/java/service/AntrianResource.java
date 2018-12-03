2104bd39b47a6f35ab6cf6f29e942a353861e1c1
Wewen <Wewen@Wewen-PC>
2018-12-03 23:36:21 +0700

    Version 0.18

--- 165314011_RSKU/src/java/service/AntrianResource.java
+++ 165314011_RSKU/src/java/service/AntrianResource.java
@@ -5,6 +5,8 @@
  */
 package service;
 
+import helper.AntrianHelper;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -13,6 +15,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pojos.Antrian;
+import com.google.gson.Gson;
+import javax.ws.rs.POST;
 
 /**
  * REST Web Service
@@ -33,21 +39,46 @@ public class AntrianResource {
 
     /**
      * Retrieves representation of an instance of service.AntrianResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
+    public Response getJson() {
         //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        AntrianHelper helper = new AntrianHelper();
+        List<Antrian> list = helper.getAllAntrian();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return Response.status(200)
+                .entity(json)
+                .build();
     }
 
+    @POST
+    @Path("addAntrian")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addNewAntrian(String data) {
+        Gson gson = new Gson();
+        Antrian antrian = gson.fromJson(data, Antrian.class);
+        AntrianHelper helper = new AntrianHelper();
+        helper.addNewAntrian(
+                antrian.getTanggal(),
+                antrian.getNoRm(),
+                antrian.getNama(),
+                antrian.getAlamat(),
+                antrian.getNamaKlinik());
+        return Response.status(200)
+                .entity(antrian)
+                .build();
+    }
     /**
      * PUT method for updating or creating an instance of AntrianResource
+     *
      * @param content representation for the resource
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
-    }
+//    @PUT
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public void putJson(String content) {
+//    }
 }
